f30f5edb1e1b23a017296fcecc8ac78052bda89c
Utku Karadeniz <utkukaradeniz97@gmail.com>
2023-01-31 22:10:36 +0100

    refactored DWA.py(not tested yet)

--- simple_dwa_lidar/src/DWA.py
+++ simple_dwa_lidar/src/DWA.py
@@ -1,178 +1,266 @@
+"""
+Adapted from: https://github.com/AtsushiSakai/PythonRobotics/blob/master/PathPlanning/DynamicWindowApproach/dynamic_window_approach.py
+
+Reading the Code and its documentation:
+- All coordinates are given/worked upon as world coordinates unless stated otherwise in the documentation/comments.
+- Square brackets('[', ']') in comments and documentation refer to a numpy array unless stated otherwise.
+"""
 import warnings
 import math
 import numpy as np
-from config import ConfigRobot, ConfigSim, RobotType
+from config import ConfigRobot, ConfigNav, RobotType
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class DWA:
-    def __init__(self, sim_config: ConfigSim, num_agent: int):
+    """
+        Class with a straightforward Dynamic Window Approach implementation.
+    """
+    def __init__(self, sim_config: ConfigNav, num_agent: int):
+        """
+        Simple Constructor to initialize relevant parameters.\n
+        :param sim_config: configuration object for the chosen robot and navigation params
+        :param num_agent: max. number of agents to keep track of
+        """
         self.robot_model = sim_config.robot_model
-        self.init_Flag = True
         self.num_agent = num_agent
         self.config = sim_config
-        self.num_agent = num_agent
 
-    def dwa_control(self, x, config, goal, ob):
+    def dwa_control(self, r_curr_state: np.ndarray, r_goal_xy: np.ndarray, obs_xy: np.ndarray):
         """
-        Dynamic Window Approach control
+        Main function for Dynamic Window Approach that calls other necessary functions.\n
+        :param r_curr_state: current state of the robot given as: [x(m), y(m), omega(rad), v_r(m/s), v_omega(rad/s)]
+        :param r_goal_xy: goal of the robot
+        :param obs_xy: current positions of all detected/filtered obstacles
+        :return:
         """
-        dw = self.calc_dynamic_window(x, config)
+        # dynamic window returned here as a list: [v_r_min, v_r_max, v_omega_min, v_omega_max]
+        dyn_window = self.calc_dynamic_window(r_curr_state)
 
-        u, trajectory = self.calc_control_and_trajectory(x, dw, config, goal, ob)
-        return u, trajectory
+        proposed_action, trajectory = self.calc_control_and_trajectory(r_curr_state, dyn_window, r_goal_xy, obs_xy)
+        return proposed_action, trajectory
 
-    def motion(self, x, u, dt):
+    @staticmethod
+    def motion(r_state_in_traj: np.ndarray, proposed_action: list, time_tick: float) -> np.ndarray:
         """
-        motion model
+        The motion model for DWA. Calculates the next state of the robot with given parameters.\n
+        :param r_state_in_traj: state of the robot for the CURRENT TRAJECTORY UNDER CONSIDERATION given as:
+        [x(m), y(m), omega(rad), v_r(m/s), v_omega(rad/s)]
+        :param proposed_action: linear(translational) and angular velocity, both in [m/s], given as a list of two floats
+        :param time_tick: floating point number to indicate how long into the future we are sampling(see ConfigSim)
+        :return: next state of the robot given as: [x(m), y(m), omega(rad), v_r(m/s), v_omega(rad/s)]
         """
-        # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
-        # TODO: debug this function
-        # u -> linear(x) and angular vel.(z) action / vel. -> 2 wheels needed to created curves
-        # x -> current state for given time-step
-        # publish u to cmd_vel
-        x[2] += u[1] * dt
-        x[0] += u[0] * math.cos(x[2]) * dt
-        x[1] += u[0] * math.sin(x[2]) * dt
-        x[3] = u[0]
-        x[4] = u[1]
+        # extract the proposed linear and angular velocities
+        prop_lin_vel = proposed_action[0]  # [m/s]
+        prop_ang_vel = proposed_action[1]  # [m/s]
 
-        return x
+        # x_new = v_r * cos(omega) * delta_t, with v_r * cos(omega) = v_x
+        r_state_in_traj[0] += prop_lin_vel * math.cos(r_state_in_traj[2]) * time_tick
 
-    def calc_dynamic_window(self, x, config):
+        # y_new = v_r * sin(omega) * delta_t, with v_r * sin(omega) = v_y
+        r_state_in_traj[1] += prop_lin_vel * math.sin(r_state_in_traj[2]) * time_tick
+
+        r_state_in_traj[2] += prop_ang_vel * time_tick  # omega_new = v_omega * delta_t
+        r_state_in_traj[3] = prop_lin_vel  # v_r_new = proposed linear velocity
+        r_state_in_traj[4] = prop_ang_vel  # v_omega_new = proposed angular velocity
+
+        return r_state_in_traj
+
+    def calc_dynamic_window(self, r_curr_state: np.ndarray) -> list:
         """
-        calculation dynamic window based on current state x
+        Function to calculate the Dynamic Window based on current state r_curr_state.\n
+        :param r_curr_state: current state of the robot given as: [x(m), y(m), omega(rad), v_r(m/s), v_omega(rad/s)]
+        :return: Dynamic Window with allowed linear and angular velocity thresholds as a list: [v_r_min, v_r_max, v_omega_min, v_omega_max]
         """
 
         # Dynamic window from robot specification
-        Vs = [config.min_speed, config.max_speed,
-              -config.max_yaw_rate, config.max_yaw_rate]
+        v_allowed = [self.config.min_speed, self.config.max_speed,
+                     -self.config.max_yaw_rate, self.config.max_yaw_rate]
 
         # Dynamic window from motion model
-        Vd = [x[3] - config.max_accel * config.dt,
-              x[3] + config.max_accel * config.dt,
-              x[4] - config.max_delta_yaw_rate * config.dt,
-              x[4] + config.max_delta_yaw_rate * config.dt]
+        v_curr_possible = [r_curr_state[3] - self.config.max_accel * self.config.dt,
+                           r_curr_state[3] + self.config.max_accel * self.config.dt,
+                           r_curr_state[4] - self.config.max_delta_yaw_rate * self.config.dt,
+                           r_curr_state[4] + self.config.max_delta_yaw_rate * self.config.dt]
 
-        #  [v_min, v_max, yaw_rate_min, yaw_rate_max]
-        dw = [max(Vs[0], Vd[0]), min(Vs[1], Vd[1]),
-              max(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
+        # Dynamic Window calculated | allowed vel. vs. currently possible vel.
+        dyn_window = [max(v_allowed[0], v_curr_possible[0]), min(v_allowed[1], v_curr_possible[1]),
+                      max(v_allowed[2], v_curr_possible[2]), min(v_allowed[3], v_curr_possible[3])]
 
-        return dw
+        return dyn_window
 
-    def predict_trajectory(self, x_init, v, y, config):
+    def predict_trajectory(self, r_curr_state: np.ndarray, v_r: float, v_omega: float):
         """
-        predict trajectory with an input
+        Predicts the trajectory(array of sequential robot states) within the prediction window.\n
+        :param r_curr_state: current state of the robot given as: [x(m), y(m), omega(rad), v_r(m/s), v_omega(rad/s)]
+        :param v_r: input linear(translational) velocity
+        :param v_omega: input angular velocity
+        :return: predicted trajectory(array of all robot states in sequential order)
         """
+        # copy current robot state. r_state_in_traj is to be used for iteratively calculating the next state of the
+        # robot within the prediction
+        r_state_in_traj = np.array(r_curr_state)
 
-        x = np.array(x_init)
-        trajectory = np.array(x)
+        # array to store all states of robot for the current trajectory under consideration
+        trajectory = np.array(r_curr_state)
+
         time = 0
-        while time <= config.predict_time:
-            x = self.motion(x, [v, y], config.dt)
+        while time <= self.config.predict_time:
+            # calculate next state of the robot inside the prediction
+            x = self.motion(r_state_in_traj, [v_r, v_omega], self.config.dt)
+
+            # append to the array of all states
             trajectory = np.vstack((trajectory, x))
-            time += config.dt
+
+            time += self.config.dt
 
         return trajectory
 
-    def calc_to_goal_cost(self, trajectory, goal):
+    @staticmethod
+    def calc_to_goal_cost(trajectory: np.ndarray, r_goal_xy: np.ndarray) -> float:
         """
-            calc to goal cost with angle difference
+        Calculates the cost to the goal with respect to the angle difference.\n
+        :param trajectory: array of all robot states for current trajectory under consideration
+        :param r_goal_xy: goal of the robot
+        :return:
         """
-        dx = goal[0] - trajectory[-1, 0]
-        dy = goal[1] - trajectory[-1, 1]
-        error_angle = math.atan2(dy, dx)
-        cost_angle = error_angle - trajectory[-1, 2]
-        cost = abs(math.atan2(math.sin(cost_angle), math.cos(cost_angle)))
+        # calc distance to goal using last state of robot in pred. frame
+        dx = r_goal_xy[0] - trajectory[-1, 0]
+        dy = r_goal_xy[1] - trajectory[-1, 1]
+
+        # using arctan, calculate the angle between the current position of the robot and the goal(aka. the optimal
+        # angle, the trajectory should have
+        optimal_angle = math.atan2(dy, dx)  # [rad]
+
+        # calc the difference between the optimal and current angle
+        cost_angle = optimal_angle - trajectory[-1, 2]  # [rad]
+
+        # original implementation's line below, which is equivalent to just taking the absolute value of the cost_angle
+        # cost = abs(math.atan2(math.sin(cost_angle), math.cos(cost_angle)))  # [rad]
+        # we are interested in how much the diff. is, not in which direction the difference is -> so, take absolute val.
+        cost = abs(cost_angle)
 
         return cost
 
-    def calc_obstacle_cost(self, trajectory, ob, config):
+    def calc_obstacle_cost(self, trajectory: np.ndarray, obs_xy: np.ndarray):
         """
-        calc obstacle cost inf: collision
+        Calculates the cost to the goal with respect to the angle difference.\n
+        :param trajectory: numpy array of all robot states for "current" prediction(aka. trajectory of robot in prediction)
+        :param obs_xy: numpy array of all positions of every obstacle in world coordinate system
+        :return: dist of the closest obstacle to the robot (specifically: 1 / min_r)
         """
-        ox = ob[:, :, 0]
-        oy = ob[:, :, 1]
+        # extract x and y positions of all obstacles
+        ox = obs_xy[:, :, 0]
+        oy = obs_xy[:, :, 1]
 
+        # calculate dist to the robot(in every robot state for the current trajectory) in x and y axis
         dx = trajectory[:, None, 0] - ox
         dy = trajectory[:, None, 1] - oy
-        r = np.hypot(dx, dy)
 
-        if config.robot_type == RobotType.circle:
-            if np.array(r <= config.robot_radius).any():
+        dist_to_obs = np.hypot(dx, dy)
+
+        if self.config.robot_type == RobotType.circle:
+            # collision check
+            if np.array(dist_to_obs <= self.config.robot_radius).any():
                 return float("Inf")
         else:
-            print("Chose the wrong Robot Type, bruh moment")
-            exit(-1)
+            raise ValueError("Current implementation of the code only works with circular robot type!")
 
-        min_r = np.nanmin(r)
+        # if no collision, choose the obstacle closest to the robot
+        min_r = np.nanmin(dist_to_obs)
 
         return 1.0 / min_r  # OK
 
-    def calc_control_and_trajectory(self, x, dw, config, goal, ob):
+    def calc_control_and_trajectory(self, r_curr_state: np.ndarray, dyn_window: list, r_goal_xy: np.ndarray,
+                                    obs_xy: np.ndarray):
         """
-        calculation final input with dynamic window
+        Calculates the best proposed action and the trajectory related to that action.\n
+        :param r_curr_state: current state of the robot given as: [x(m), y(m), omega(rad), v_r(m/s), v_omega(rad/s)]
+        :param dyn_window: Dynamic Window with allowed linear and angular velocity thresholds as a list:
+        [v_r_min, v_r_max, v_omega_min, v_omega_max]
+        :param r_goal_xy: goal of the robot given as: [x(m), y(m)]
+        :param obs_xy: current positions of all detected/filtered obstacles
+        :return:
         """
-        x_init = x[:]
+        # init variables
+        r_curr_state_init = np.copy(r_curr_state)
         min_cost = float("inf")
-        best_u = [0.0, 0.0]
-        best_trajectory = np.array([x])
+        best_proposed_action = [0.0, 0.0]
+        best_trajectory = np.array([r_curr_state])
 
-        # evaluate all trajectory with sampled input in dynamic window
-        for v in np.arange(dw[0], dw[1], config.v_resolution):
-            for y in np.arange(dw[2], dw[3], config.yaw_rate_resolution):
+        # evaluate all trajectories with sampled input(acc. resolutions from config.) in dynamic window
+        for v_r in np.arange(dyn_window[0], dyn_window[1], self.config.v_resolution):
+            for v_omega in np.arange(dyn_window[2], dyn_window[3], self.config.yaw_rate_resolution):
 
-                trajectory = self.predict_trajectory(x_init, v, y, config)
-                # calc cost
-                to_goal_cost = config.to_goal_cost_gain * self.calc_to_goal_cost(trajectory, goal)
-                speed_cost = config.speed_cost_gain * (config.max_speed - trajectory[-1, 3])
-                ob_cost = config.obstacle_cost_gain * self.calc_obstacle_cost(trajectory, ob, config)
-                # print("goal:" + str(to_goal_cost))
-                # print("speed:" + str(speed_cost))
-                # print("ob:" + str(ob_cost))
-                # print("-------")
+                # calculate predicted trajectory of the robot
+                trajectory = self.predict_trajectory(r_curr_state_init, v_r, v_omega)
+
+                ### calc cost of the current "predicted" trajectory
+
+                # calc. the angle difference between the current "predicted" trajectory's angle and the optimal angle
+                # to reach the goal (optimal case: robot is looking directly at the goal)
+                to_goal_cost = self.config.to_goal_cost_gain * self.calc_to_goal_cost(trajectory, r_goal_xy)
+
+                # calc. the difference between the linear velocity of the current "predicted" trajectory and the max
+                # allowed linear velocity (optimal case: robot is moving at full speed to the goal)
+                speed_cost = self.config.speed_cost_gain * (self.config.max_speed - trajectory[-1, 3])
+
+                # calc. 1 / dist. to closest obstacle
+                ob_cost = self.config.obstacle_cost_gain * self.calc_obstacle_cost(trajectory, )
+
+                # total cost of the trajectory
                 final_cost = to_goal_cost + speed_cost + ob_cost
+
                 # search minimum trajectory
                 if min_cost >= final_cost:
                     min_cost = final_cost
-                    best_u = [v, y]
+                    best_proposed_action = [v_r, v_omega]
                     best_trajectory = trajectory
-                    if abs(best_u[0]) < config.robot_stuck_flag_cons \
-                            and abs(x[3]) < config.robot_stuck_flag_cons:
-                        # to ensure the robot do not get stuck in
-                        # best v=0 m/s (in front of an obstacle) and
-                        # best omega=0 rad/s (heading to the goal with
-                        # angle difference of 0)
-                        best_u[1] = -config.max_yaw_rate  # Bug in original code? Corrected to max_yaw_rate instead
-                        # max_delta_yaw_rate
-        return best_u, best_trajectory
+                    # if current v_r ~ 0 and best v_r ~ 0, robot is stuck (e.g. in front of an obstacle) => make him
+                    # turn around, so he can calculate other trajectories
+                    if abs(best_proposed_action[0]) < self.config.robot_stuck_flag_cons \
+                            and abs(r_curr_state[3]) < self.config.robot_stuck_flag_cons:
+                        # to ensure the robot does not get stuck in best v=0 m/s (in front of an obstacle) and
+                        # best omega=0 rad/s (heading to the goal with angle difference of 0)
+                        best_proposed_action[1] = -self.config.max_yaw_rate  # Bug in original code? Corrected to max_yaw_rate instead
 
-    def cart2pol(self, x, y):
-        """
-        Function to convert cartesian coordinates into polar coordinates.
-        """
-        rho = np.sqrt(x ** 2 + y ** 2)
-        phi = np.arctan2(y, x)
-        return rho, phi
+        return best_proposed_action, best_trajectory
 
-    def pol2cart(self, rho, phi):
+    @staticmethod
+    def cart2pol(x: float, y: float) -> tuple:
         """
-        Function to convert polar coordinates into cartesian coordinates.
+        Function to convert cartesian coordinates into polar coordinates.\n
+        :param x: position in x-axis as floating point number
+        :param y: position in y-axis as floating point number
+        :return: distance to origin and angle returned as tuple of two floats respectively
         """
-        x = rho * np.cos(phi)
-        y = rho * np.sin(phi)
+        r = np.sqrt(x ** 2 + y ** 2)
+        theta = np.arctan2(y, x)
+        return r, theta
+
+    @staticmethod
+    def pol2cart(r: float, theta: float) -> tuple:
+        """
+        Function to convert polar coordinates into cartesian coordinates.\n
+        :param r: distance to origin as floating point number
+        :param theta: angle to x-axis as floating point number
+        :return: position in x-axis and y-axis returned as tuple of two floats respectively
+        """
+        x = r * np.cos(theta)
+        y = r * np.sin(theta)
         return x, y
 
     def predict(self, obs_traj_pos: np.ndarray, obs_lidar, r_pos_xy: np.ndarray, r_goal_xy: np.ndarray, r_vel_state: np.ndarray) -> np.ndarray:
         """
-        # r_state -> v_x, v_y, orientation, a_x, v_orientation
-        r_state = r_state
-        # r_pos -> x, y
-        r_state_xy = r_pos
-        # r_goal
-        r_goal = r_goal
-
+        Main function for Dynamic Window Approach that calls other necessary functions to predict trajectories and
+        choose the best proposed action.\n
+        :param obs_traj_pos: position of all obstacles given as: [[x(m), y(m)], ...]
+        :param r_pos_xy: current position of the robot given as: [x(m), y(m)]
+        :param r_goal_xy: goal of the robot given as: [x(m), y(m)]
+        :param r_vel_state: current velocity and orientation of robot given as: [v_x(m/s), v_y(m/s), theta(rad),
+        v_lin(m/s), v_ang(rad/s)]
+        :return: best proposed action as a numpy array
         """
         # current state of the robot given as: [x(m), y(m), theta(rad), v_lin(m/s), v_ang(rad/s)]
         r_curr_state = np.concatenate([r_pos_xy, r_vel_state[2:]])
@@ -185,5 +273,5 @@ class DWA:
 
 if __name__ == '__main__':
     robot_config = ConfigRobot(robot_model="locobot", collision_dist=0.2)
-    sim_config = ConfigSim(robot_config=robot_config)
+    sim_config = ConfigNav(robot_config=robot_config)
     dwa = DWA(sim_config, -1, 30)
